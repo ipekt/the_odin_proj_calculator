f619b964da2c59dd42abb679fd6f38c612269c39
Ipek Turgul <ipek.turgul@hiveempire.com>
2015-11-12 18:51:03 +1100

    setup testing

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 /* JS On Screen Calculator
- 
   Project: http://www.theodinproject.com/javascript-and-jquery/on-screen-calculator
 */
 
-$(function () {
+function ready(calculateMe) {
+  if (document.readyState != 'loading'){
+    calculate();
+  } else {
+    document.addEventListener('DOMContentLoaded', calculateMe);
+  }
+}
+
+var calculateMe = function () {
+
   var number1 = '',
     number2 = '',
     operator, operator_after, result,
     numSet = false,
     operatorFirstSet = false;
 
-  // Calculations 
-  function add(x, y) {
-    return x + y;
-  }
-
-  function subtract(x, y) {
-    return x - y;
-  }
-
-  function multiply(x, y) {
-    return x * y;
-  }
-
-  function divide(x, y) {
-    return x / y;
-  }
-
+  var operators = {
+    add: function (x, y) {
+      return x + y;
+    },
+    subtract: function (x, y) {
+      return x - y;
+    },
+    multiply: function (x, y) {
+      return x * y;
+    },
+    divide: function (x, y) {
+      return x / y;
+    }
+  };
+ 
   // Calculates user inputs 
   var calculate = function (number1, number2, operator) {
     number1 = parseFloat(number1, 10);
@@ -34,98 +40,108 @@ $(function () {
 
     switch (operator) {
     case "+":
-      result = add(number1, number2);
+      result = operators.add(number1, number2);
       break;
     case "-":
-      result = subtract(number1, number2);
+      result = operators.subtract(number1, number2);
       break;
     case "/":
-      result = divide(number1, number2);
+      result = operators.divide(number1, number2);
       break;
     case "x":
-      result = multiply(number1, number2);
+      result = operators.multiply(number1, number2);
       break;
     }
   };
 
   // Displays result on screen
   var displayResults = function (result) {
-    $('.screen').text(result);
+    document.querySelector('.screen').innerHTML = result;
   };
 
   var reset = function () {
     number1 = '';
     number2 = '';
     operator = '';
-    $('.screen').text('0');
+    document.querySelector('.screen').innerHTML = '0';
     numSet = false;
     operatorFirstSet = false;
   };
 
   // Click event registered to numbers
-  $('.number').on('click', function () {
-    // First number is set here: 
-    if (!numSet) {
+  var numbers = document.querySelectorAll('.number');
+  
+  for (var i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener('click', function () {
+
+      var selectedNum = this.innerHTML; 
+
+      if (!numSet) {
       // Dont let user put more than one period in a number
-      if (number1.indexOf('.') > -1 && $(this).text() === '.') {
-        displayResults(number1);
-      } else {
-        number1 += $(this).text();
-        displayResults(number1);
-      }
+        if (number1.indexOf('.') > -1 && selectedNum === '.') {
+          displayResults(number1);
+        } else {
+          number1 += selectedNum;
+          displayResults(number1);
+        }
       // Number 2 is set here:
     } else {
       // Dont let user put more than one period in a number
-      if (number2.indexOf('.') > -1 && $(this).text() === '.') {
+      if (number2.indexOf('.') > -1 && selectedNum === '.') {
         displayResults(number2);
       } else {
-        number2 += $(this).text();
+        number2 += selectedNum;
         displayResults(number2);
       }
     }
-  });
+    });
+  }
 
-  // Set operator click event
-  $('.operator').on('click', function () {
 
-    // First operator is set here
-    if (!operatorFirstSet && number1 === '') {
+  // Click event registered to operators
+  var operatorElements = document.querySelectorAll('.operator');
+  
+  for (var j = 0; j < operatorElements.length; j++) {
+    operatorElements[j].addEventListener('click', function () {
 
-      displayResults(0);
+      var selectedOperator = this.innerHTML;
+      
+      if (!operatorFirstSet && number1 === '') {
 
-    } else if (!operatorFirstSet) {
+        displayResults(0);
 
-      operator = $(this).text();
-      displayResults(number1);
-      operatorFirstSet = true;
-      numSet = true;
+      } else if (!operatorFirstSet) {
 
-      // Set first operator to user's last click
-    } else if (number2 === '') {
-      operator = $(this).text();
-      displayResults(number1);
-    }
-    // Other operators are set here:
-    else {
+        operator = selectedOperator;
+        displayResults(number1);
+        operatorFirstSet = true;
+        numSet = true;
 
-      calculate(number1, number2, operator);
-      operator = $(this).text();
-      //Display result when next operator is set. 
-      displayResults(result);
-      // Assign number 1 to be result
-      number1 = result;
-      number2 = '';
-    }
+        // Set first operator to user's last click
+      } else if (number2 === '') {
+        operator = selectedOperator;
+        displayResults(number1);
+      }
+      // Other operators are set here:
+      else {
+        calculate(number1, number2, operator);
+        operator = selectedOperator;
+        //Display result when next operator is set. 
+        displayResults(result);
+        // Assign number 1 to be result
+        number1 = result;
+        number2 = '';
+      }
+    });
+  }
 
-  });
-
-  $('.calculate').on('click', function () {
+  document.querySelector('.calculate').addEventListener('click', function () {
     calculate(number1, number2, operator);
     displayResults(result);
   });
 
-  $('.reset').on('click', function () {
-    reset();
-  });
+  document.querySelector('.reset').addEventListener('click', reset);
 
-});
+}
+
+ready(calculateMe);
